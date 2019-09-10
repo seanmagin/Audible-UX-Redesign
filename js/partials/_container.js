a97a92fe837e19ec6b97cebb0e3473ffd246968f
Sean Magin <smagin.work@gmail.com>
2019-09-10 15:47:06 +1000

    fixed app container

--- js/partials/_container.js
+++ js/partials/_container.js
@@ -1,11 +1,3 @@
 function appContainer() {
-    document.getElementById("section").innerHTML =
-        `<div class="container">
-            <div class="row">
-                <div class="col-sm-6 offset-sm-3">
-                    <div id="bookSearch"></div>
-                </div>
-            </div>
-            <h1>Hello, world!</h1>
-        </div>`
+
 }
